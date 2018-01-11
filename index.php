e77bd9faf5306ca873480f65e09316810ee5fd0f
Raine Padilla <epadilla7@mail.greenriver.edu>
2018-01-11 14:25:01 -0500

    made project fatfree

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    require_once ('vendor/autoload.php');
+
+    $f3 = Base::instance();
+
+    $f3->route('GET /', function ()
+    {
+        $view = new View();
+        echo $view->render('views/home.html');
+    });
+
+    $f3->run();
